a8439b5325fe2d88831376012aa13a2c7ef5a1d0
MADEIRA Romann <rmadeira@iutbayonne.univ-pau.fr>
2021-01-10 17:26:25 +0100

    Ajout réalisé avec succès

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,13 +5,19 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Entreprise;
+use App\Entity\Formation;
+use App\Entity\Stage;
+
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         //Creation d'un générateur de données Faker
         $faker = \Faker\Factory::create('fr_FR');
+
         $nbEntreprises = 15;
+        $nbStages= 15;
+        $nbFormations = 15;
 
         for($i=0; $i < $nbEntreprises; $i++){
             $entreprise = new Entreprise();
@@ -19,6 +25,21 @@ class AppFixtures extends Fixture
             $entreprise->setActivite($faker->jobTitle);
             $entreprise->setAdresse($faker->address);
             $manager->persist($entreprise);
+        }
+
+        for($i=0; $i < $nbStages; $i++){
+            $stage = new Stage();
+            $stage->setIntitule($faker->jobTitle);
+            $stage->setMission($faker->realText($maxNbChars = 50, $indexSize = 2));
+            $stage->setAdresseMail($faker->email);
+            $manager->persist($stage);
+
+        }
+        for($i=0; $i < $nbFormations; $i++){
+            $formation = new Formation();
+            $formation->setNom("DUT Info");
+            $formation->setDescription($faker->realText($maxNbChars = 50, $indexSize = 2));
+            $manager->persist($formation);
         }
         $manager->flush();
     }
