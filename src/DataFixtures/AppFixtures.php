ec860da44a67fcca4be0afbc94369e9bf8fbb228
MADEIRA Romann <rmadeira@iutbayonne.univ-pau.fr>
2021-01-12 18:33:49 +0100

    Modification des données de test pour inclure l'id des entrepridans les stages

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -16,8 +16,7 @@ class AppFixtures extends Fixture
         $faker = \Faker\Factory::create('fr_FR');
 
         $nbEntreprises = 15;
-        $nbStages= 15;
-        $nbFormations = 15;
+        $nbFormations = 3;
 
         for($i=0; $i < $nbEntreprises; $i++){
             $entreprise = new Entreprise();
@@ -25,16 +24,20 @@ class AppFixtures extends Fixture
             $entreprise->setActivite($faker->jobTitle);
             $entreprise->setAdresse($faker->address);
             $manager->persist($entreprise);
+
+            $nbStages = $faker->numberBetween($min=0, $max=2);
+
+            for($ii=0; $ii < $nbStages; $ii++){
+                $stage = new Stage();
+                $stage->setIntitule($faker->jobTitle);
+                $stage->setMission($faker->realText($maxNbChars = 50, $indexSize = 2));
+                $stage->setAdresseMail($faker->email);
+                $entreprise->addStage($stage);
+                $manager->persist($entreprise);
+                $manager->persist($stage);
+            }
         }
 
-        for($i=0; $i < $nbStages; $i++){
-            $stage = new Stage();
-            $stage->setIntitule($faker->jobTitle);
-            $stage->setMission($faker->realText($maxNbChars = 50, $indexSize = 2));
-            $stage->setAdresseMail($faker->email);
-            $manager->persist($stage);
-
-        }
         for($i=0; $i < $nbFormations; $i++){
             $formation = new Formation();
             $formation->setNom("DUT Info");
