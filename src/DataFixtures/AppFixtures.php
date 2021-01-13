56f2faa6e65bea335fa461dfb3fbe3821ac1b0c2
MADEIRA Romann <rmadeira@iutbayonne.univ-pau.fr>
2021-01-13 23:50:25 +0100

    Mise a jour des noms des stages sur la page des informations de stage

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -15,8 +15,25 @@ class AppFixtures extends Fixture
         //Creation d'un générateur de données Faker
         $faker = \Faker\Factory::create('fr_FR');
 
+        $dutInfo = new Formation();
+        $dutInfo->setNom("DUT Informatique");
+        $dutInfo->setDescription("Le BUT informatique est un diplôme de grade licence qui prépare aux métiers de l’informatique tout en répondant aux exigences des entreprises.");
+
+        $licencePro = new Formation();
+        $licencePro->setNom("Licence Professionnelle Multimédia");
+        $licencePro->setDescription("Une licence professionnelle en multimédia est un cursus d’un an qui a pour but de former les étudiants aux métiers de la création, de la conception et de la réalisation de produits et de services multimédias.");
+
+        $duTIC = new Formation();
+        $duTIC->setNom("DU TIC");
+        $duTIC->setDescription("Technologies de l’information et de la communication - s’adresse à toute personne en reconversion professionnelle ou en recherche de compléments de compétences dans les domaines du web, du multimédia, du webmarketing et de la gestion de projets.");
+
+        $tableauFormations = array($dutInfo, $licencePro, $duTIC);
+
+        foreach ($tableauFormations as $formation){
+            $manager->persist($formation);
+        }
+
         $nbEntreprises = 15;
-        $nbFormations = 3;
 
         for($i=0; $i < $nbEntreprises; $i++){
             $entreprise = new Entreprise();
@@ -32,17 +49,14 @@ class AppFixtures extends Fixture
                 $stage->setIntitule($faker->jobTitle);
                 $stage->setMission($faker->realText($maxNbChars = 50, $indexSize = 2));
                 $stage->setAdresseMail($faker->email);
+                $numFormation = $faker->numberBetween($min=0, $max=2);
+                $tableauFormations[$numFormation] -> addStage($stage);
                 $entreprise->addStage($stage);
                 $manager->persist($entreprise);
+                $manager->persist($tableauFormations[$numFormation]);
                 $manager->persist($stage);
-            }
-        }
 
-        for($i=0; $i < $nbFormations; $i++){
-            $formation = new Formation();
-            $formation->setNom("DUT Info");
-            $formation->setDescription($faker->realText($maxNbChars = 50, $indexSize = 2));
-            $manager->persist($formation);
+            }
         }
         $manager->flush();
     }
