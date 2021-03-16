d6aa1b23c76d87277db87c835dedd36fbc639a87
MADEIRA Romann <rmadeira@iutbayonne.univ-pau.fr>
2021-03-16 16:35:10 +0100

    Ajout du formulaire d'inscription, changement des logos du site

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Form\UserType;
+use App\Entity\User;
 
 class SecurityController extends AbstractController
 {
@@ -33,4 +38,26 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    /**
+     * @Route("/register", name="app_register", methods={"GET","POST"})
+     */
+    public function inscription(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //$entityManager = $this->getDoctrine()->getManager();
+            //$entityManager->persist($user);
+            //$entityManager->flush();
+
+            return $this->redirectToRoute('stage_index');
+        }
+
+        return $this->render('security/register.html.twig', ['form' => $form->createView()]);
+    }
+
+
 }
